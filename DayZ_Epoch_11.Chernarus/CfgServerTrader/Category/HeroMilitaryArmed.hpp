3ee003c3c3a462c194476089c97cb93817256807
Casey <Caseycooper843@gmail.com>
2016-06-23 13:42:46 -0400

    adding more vehicles to hero trader

--- DayZ_Epoch_11.Chernarus/CfgServerTrader/Category/HeroMilitaryArmed.hpp
+++ DayZ_Epoch_11.Chernarus/CfgServerTrader/Category/HeroMilitaryArmed.hpp
@@ -1,13 +1,43 @@
 class Category_562 {
-	class HMMWV_M998A2_SOV_DES_EP1_DZE {
+	class HMMWV_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] = {5,"ItemGoldBar10oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+		class HMMWV_M1035_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] = {5,"ItemGoldBar10oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class HMMWV_M998A2_SOV_DES_EP1 {
 		type = "trade_any_vehicle";
 		buy[] = {1,"ItemBriefcase100oz"};
 		sell[] = {5,"ItemGoldBar10oz"};
 	};
-	class HMMWV_M1151_M2_CZ_DES_EP1_DZE {
+	class HMMWV_M1151_M2_DES_EP1 {
 		type = "trade_any_vehicle";
 		buy[] = {4,"ItemBriefcase100oz"};
 		sell[] = {1,"ItemBriefcase100oz"};
+	};
+	class HMMWV_MK19_DES_EP1{
+		type = "trade_any_vehicle";
+		buy[] = {1,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class HMMWV_M998_crows_M2_DES_EP1{
+		type = "trade_any_vehicle";
+		buy[] = {5,"ItemBriefcase100oz"};
+		sell[] = {2,"ItemBriefcase100oz"};
+	};	
+	class MTVR_Reammo_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] = {5,"ItemGoldBar10oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class MTVR_Repair_DES_EP1 {
+		type = "trade_any_vehicle";
+		buy[] = {5,"ItemGoldBar10oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
 	};
 	class LandRover_Special_CZ_EP1_DZE {
 		type = "trade_any_vehicle";
@@ -29,6 +59,16 @@ class Category_562 {
 		buy[] = {2,"ItemBriefcase100oz"};
 		sell[] = {1,"ItemBriefcase100oz"};
 	};
+	class BAF_Jackal2_L2A1_W{
+		type = "trade_any_vehicle";
+		buy[] = {10,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemBriefcase100oz"};
+	};
+	class Ural_ZU23_TK_EP1{
+		type = "trade_any_vehicle";
+		buy[] = {10,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemBriefcase100oz"};
+	};
 	class BTR90_DZ {
 		type = "trade_any_vehicle";
 		buy[] = {12,"ItemBriefcase100oz"};
@@ -44,24 +84,14 @@ class Category_562 {
 		buy[] = {6,"ItemBriefcase100oz"};
 		sell[] = {3,"ItemBriefcase100oz"};
 	};
-	class HMMWV_MK19_DES_EP1{
-		type = "trade_any_vehicle";
-		buy[] = {1,"ItemBriefcase100oz"};
-		sell[] = {5,"ItemGoldBar10oz"};
-	};
 	class M1126_ICV_M2_EP1{
 		type = "trade_any_vehicle";
 		buy[] = {8,"ItemBriefcase100oz"};
 		sell[] = {4,"ItemBriefcase100oz"};
 	};
-	class BAF_Jackal2_L2A1_W{
+	class M1128_MGS_EP1{
 		type = "trade_any_vehicle";
-		buy[] = {10,"ItemBriefcase100oz"};
-		sell[] = {5,"ItemBriefcase100oz"};
+		buy[] = {15,"ItemBriefcase100oz"};
+		sell[] = {4,"ItemBriefcase100oz"};
 	};
-    class HMMWV_M998_crows_M2_DES_EP1{
-		type = "trade_any_vehicle";
-		buy[] = {5,"ItemBriefcase100oz"};
-		sell[] = {2,"ItemBriefcase100oz"};
-	};	
 };
